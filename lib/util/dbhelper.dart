bbb4121239fa7f545ad2e01b2d6f2f67ac66da19
kayuyu.mwaura <kayuyu.mwaura@tovutigroup.com>
2020-07-14 10:39:16 +0300

    install dependencies(pubspec) add db

---
+++ lib/util/dbhelper.dart
@@ -0,0 +1,46 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_app/model/todo.dart';
+
+
+//call dbhelper once. use singleton
+class DbHelper {
+  static final DbHelper _dbHelper = DbHelper._internal();
+  String tblTodo = "todo";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
+
+DbHelper._internal();
+
+factory DbHelper() {
+  return _dbHelper;
+}
+
+static Database _db;
+
+Future<Database> get db async {
+  if (_db == null) {
+    _db = await initializeDb();
+  }
+  return _db;
+}
+
+Future<Database> initializeDb() async {
+  Directory dir = await getApplicationDocumentsDirectory();
+  String path = dir.path + "todos.db";
+  var dbTodos = await openDatabase(path, version: 1, onCreate: _createDb);
+  return dbTodos;
+}
+
+void _createDb(Database db, int newVersion) async {
+  await db.execute(
+    "CREATE TABLE $tblTodo($colId INTEGER PRIMARY KEY, $colTitle TEXT," + 
+    "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)");
+}
+
+}
