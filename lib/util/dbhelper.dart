ffe0ebd8652933de1285dd8147ba0257b3455f39
kayuyu.mwaura <kayuyu.mwaura@tovutigroup.com>
2020-07-14 11:54:44 +0300

    build db helper with crud functions

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -37,10 +37,44 @@ Future<Database> initializeDb() async {
   return dbTodos;
 }
 
-void _createDb(Database db, int newVersion) async {
-  await db.execute(
-    "CREATE TABLE $tblTodo($colId INTEGER PRIMARY KEY, $colTitle TEXT," + 
-    "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)");
-}
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+      "CREATE TABLE $tblTodo($colId INTEGER PRIMARY KEY, $colTitle TEXT," + 
+      "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)");
+  }
+
+  Future<int> insertTodo(Todo todo) async {
+    Database db = await this.db;
+    var result = await db.insert(tblTodo, todo.toMap());
+    return result;
+  }
+
+  Future<List> getTodos() async {
+    Database db = await this.db;
+    var result = await db.rawQuery("SELECT * FROM $tblTodo order by $colPriority ASC");
+    return result;
+  }
+
+  Future<int> getCount() async {
+    Database db = await this.db;
+    var result = Sqflite.firstIntValue(
+      await db.rawQuery("select count (*) from $tblTodo")
+    );
+    return result;
+  }
+
+  Future<int> updateTodo(Todo todo) async {
+    var db = await this.db;
+    var result = await db.update(tblTodo, todo.toMap(),
+      where: "$colId = ?", whereArgs: [todo.id]);
+    return result;
+  }
+
+  Future<int> deleteTodo(int id) async {
+    int result;
+    var db = await this.db;
+    result = await db.rawDelete('DELETE FROM $tblTodo WHERE $colId = $id');
+    return result;
+  }
 
 }
